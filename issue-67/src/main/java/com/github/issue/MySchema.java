7f5d9f6c6417b82ddc84e6bd568dd462cbd178f1
Sebastien Briquet <sebastien.briquet@amadeus.com>
2015-04-06 20:42:44 +0200

    issue-67

---
+++ issue-67/src/main/java/com/github/issue/MySchema.java
@@ -0,0 +1,21 @@
+package com.github.issue;
+
+import com.fasterxml.jackson.module.jsonSchema.types.ObjectSchema;
+
+public class MySchema extends ObjectSchema
+{
+//    public static class OID
+//    {
+//        @JsonProperty
+//        public String $oid;
+//    }
+//
+//    @JsonProperty
+//    public OID _id = null;
+//
+//    @JsonCreator
+//    public MySchema()
+//    {
+//        super();
+//    }
+}
---
+++ issue-67/src/test/java/com/github/issue/TestSchema.java
@@ -0,0 +1,49 @@
+package com.github.issue;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.issue.MySchema;
+
+public class TestSchema
+{
+	@Test
+	public void testSchema()
+	{
+		/** jackson schema */
+		String json = "{ \"type\" : \"object\" , \"id\" : \"7a2e8538-196b-423e-b714-13515848ec0c\" , \"description\" : \"My Schema\" , \"title\" : \"my-json-schema\" , \"properties\" : { \"myarray\" : { \"type\" : \"array\" , \"required\" : true , \"title\" : \"my property #2\" , \"items\" : { \"type\" : \"string\"} , \"maxItems\" : 5} , \"mystring\" : { \"type\" : \"string\" , \"required\" : true , \"title\" : \"my property #1\" , \"format\" : \"REGEX\" , \"pattern\" : \"\\\\w+\"} , \"myobject\" : { \"type\" : \"object\" , \"required\" : true , \"title\" : \"my property #3\" , \"properties\" : { \"subprop\" : { \"type\" : \"string\" , \"required\" : true , \"title\" : \"sub property #1\" , \"format\" : \"REGEX\" , \"pattern\" : \"\\\\w{3}\"}}}}}";
+
+		/** mongo like schema */
+		// String json = "{ \"_id\" : { \"$oid\" : \"55144a23771a631c581a2811\"} , \"type\" : \"object\" , \"id\" : \"7a2e8538-196b-423e-b714-13515848ec0c\" , \"description\" : \"My Schema\" , \"title\" : \"my-json-schema\" , \"properties\" : { \"myarray\" : { \"type\" : \"array\" , \"required\" : true , \"title\" : \"my property #2\" , \"items\" : { \"type\" : \"string\"} , \"maxItems\" : 5} , \"mystring\" : { \"type\" : \"string\" , \"required\" : true , \"title\" : \"my property #1\" , \"format\" : \"REGEX\" , \"pattern\" : \"\\\\w+\"} , \"myobject\" : { \"type\" : \"object\" , \"required\" : true , \"title\" : \"my property #3\" , \"properties\" : { \"subprop\" : { \"type\" : \"string\" , \"required\" : true , \"title\" : \"sub property #1\" , \"format\" : \"REGEX\" , \"pattern\" : \"\\\\w{3}\"}}}}}";
+
+		System.out.println(json);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try
+		{
+			// using value //
+
+			// ObjectSchema schema = mapper.readValue(json, ObjectSchema.class); // works
+			MySchema schema = mapper.readValue(json, MySchema.class); // fails
+
+			// using tree //
+
+			// JsonNode node = mapper.readTree(json); // works
+
+			// ObjectSchema schema = mapper.treeToValue(node, ObjectSchema.class); // works
+			// MongoSchema schema = mapper.treeToValue(node, MySchema.class); // fails
+			// ObjectSchema schema = mapper.convertValue(node, SimpleType.construct(ObjectSchema.class)); // works
+			// MongoSchema schema = mapper.convertValue(node, SimpleType.construct(MySchema.class)); // fails
+			// MongoSchema schema = mapper.convertValue(node, mapper.getTypeFactory().constructSpecializedType(SimpleType.construct(ObjectSchema.class), MySchema.class)); // fails
+
+			// output //
+
+			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(schema));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
